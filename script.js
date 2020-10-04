80a2d4a702fce2301234bc694b84f6a7476abdee
Yasin jemal <yasinjemal1234@gmail.com>
2020-10-04 23:37:18 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Get Quotes from API
+async function getQuote(){
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+
+    } catch(error){
+        console.log('whoops mammaaksi hin jiru', error);
+    }
+
+
+
+
+}
+// on load
+getQuote();
